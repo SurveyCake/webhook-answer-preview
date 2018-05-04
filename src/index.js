4eaa18d61fd7c4b394bc246d07adb85a27e8cf23
Cathy Retina 13 <a304126@gmail.com>
2018-05-04 18:04:35 +0800

    get answer data form surveycake webhook api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
-// const key = 'HASH_KEY';
-// const iv = 'IV_KEY';
-// fetch('URL_PATH')
-// 	.then((res) => res.text())
-// 	.then((dat) => {
-// 		const cipherParams = CryptoJS.lib.CipherParams.create({
-// 			ciphertext: CryptoJS.enc.Base64.parse(dat),
-// 		});
-// 		const decrypted = CryptoJS.AES.decrypt(
-// 			cipherParams,
-// 			CryptoJS.enc.Utf8.parse(key),
-// 			{
-// 				iv: CryptoJS.enc.Utf8.parse(iv),
-// 			}
-// 		);
-// 		console.log(
-// 			JSON.parse( decrypted.toString(CryptoJS.enc.Utf8) )
-// 		);
-//     });
+const parseData = ((dat, key, iv) => {
+    const cipherParams = CryptoJS.lib.CipherParams.create({
+        ciphertext: CryptoJS.enc.Base64.parse(dat),
+    });
+
+    const decrypted = CryptoJS.AES.decrypt(
+        cipherParams,
+        CryptoJS.enc.Utf8.parse(key),
+        {
+            iv: CryptoJS.enc.Utf8.parse(iv),
+        }
+    );
+
+    return JSON.parse(decrypted.toString(CryptoJS.enc.Utf8))
+});
+
+const getData = ((survey, fileHash) => (
+    fetch(`https://www.surveycake.com/webhook/v0/${survey}/${fileHash}`)
+        .then((res) => res.text())
+));
+
+const getAnswer = (() => {
+    const survey = document.getElementById('survey-hash').value;
+    const fileHash = document.getElementById('answer-hash').value;
+    const hashKey = document.getElementById('hash-key').value;
+    const hashIv = document.getElementById('hash-iv').value;
+
+    if (!survey || !fileHash || !hashKey || !hashIv) {
+        return;
+    }
+
+    getData(survey, fileHash)
+        .then((res) => parseData(res, hashKey, hashIv))
+        .then((data) => {
+            document.getElementById('result').textContent = JSON.stringify(data);
+        });
+});
+
+document.getElementById('get-data').addEventListener('click', () => {
+    getAnswer();
+});
